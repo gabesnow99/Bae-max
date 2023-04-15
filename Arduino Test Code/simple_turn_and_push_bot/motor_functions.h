50907d3f54bb4fdff219db8a0b16fdc066046b00
cpaul-mech <cpaul.mech@gmail.com>
2023-04-15 11:34:52 -0600

    new simple push bot configuration

---
+++ Arduino Test Code/simple_turn_and_push_bot/motor_functions.h
@@ -0,0 +1,153 @@
+#include <Arduino.h>
+#include <SharpIR.h>
+
+int RB = 6;	 // PIN D6 TO IN1
+int RF = 9;	 // PIN D9 TO IN2
+int LB = 10; // PIN D10 TO IN3
+int LF = 11; // PIN D11 TO IN4
+
+void stopWheels()
+{
+	analogWrite(RB, 0);
+	analogWrite(LB, 0);
+	analogWrite(RF, 0);
+	analogWrite(LF, 0);
+}
+
+void goForward(int vel = 150)
+{
+	// Serial.begin(9600);
+	Serial.println("I have reached go forward!!");
+	analogWrite(RB, 0);
+	analogWrite(LB, 0);
+	analogWrite(RF, vel);
+	analogWrite(LF, vel);
+}
+
+void goForward(int vel, int milliseconds)
+{
+	analogWrite(RB, 0);
+	analogWrite(LB, 0);
+	analogWrite(RF, vel);
+	analogWrite(LF, vel);
+	delay(milliseconds);
+	analogWrite(RB, 0);
+	analogWrite(LB, 0);
+	analogWrite(RF, 0);
+	analogWrite(LF, 0);
+}
+
+void goBackward(int vel = 150)
+{
+	analogWrite(RF, 0);
+	analogWrite(LF, 0);
+	analogWrite(RB, vel);
+	analogWrite(LB, vel);
+}
+
+void goBackward(int vel, int milliseconds)
+{
+	analogWrite(RF, 0);
+	analogWrite(LF, 0);
+	analogWrite(RB, vel);
+	analogWrite(LB, vel);
+	delay(milliseconds);
+	analogWrite(RF, 0);
+	analogWrite(LF, 0);
+	analogWrite(RB, 0);
+	analogWrite(LB, 0);
+}
+
+void turnRight(int deg = 90, int vel = 150) // going to need to test the blocking that occurs here!!
+{
+	int time = round(250 * deg / vel);
+	analogWrite(RF, 0);
+	analogWrite(LB, 0);
+	analogWrite(RB, vel);
+	analogWrite(LF, vel);
+	delay(time);
+	analogWrite(RB, 0);
+	analogWrite(LF, 0);
+}
+
+void turnLeft(int deg = 90, int vel = 150) // going to need to test the blocking that occurs here!!
+{
+	int time = round(250 * deg / vel);
+	analogWrite(LF, 0);
+	analogWrite(RB, 0);
+	analogWrite(LB, vel);
+	analogWrite(RF, vel);
+	delay(time);
+	analogWrite(LB, 0);
+	analogWrite(RF, 0);
+}
+
+void spinLeft(int vel = 150)
+{
+	analogWrite(LF, 0);
+	analogWrite(RB, 0);
+	analogWrite(LB, vel);
+	analogWrite(RF, vel);
+}
+
+void pivotLeft(int deg = 90, int vel = 150)
+{
+	int time = 2 * round(250 * deg / vel);
+	analogWrite(LF, 0);
+	analogWrite(RB, 0);
+	analogWrite(LB, 0);
+	analogWrite(RF, vel);
+	delay(time);
+	stopWheels();
+}
+
+void escapeRightBack()
+{
+	int vel = 200;
+	int faster = 250;
+	int milliseconds = 1000;
+	analogWrite(RF, faster);
+	analogWrite(LF, vel);
+	analogWrite(RB, 0);
+	analogWrite(LB, 0);
+	delay(milliseconds);
+	analogWrite(RF, 0);
+	analogWrite(LF, 0);
+	analogWrite(RB, 0);
+	analogWrite(LB, 0);
+}
+
+void escapeLeftBack()
+{
+	int vel = 200;
+	int faster = 250;
+	int milliseconds = 1000;
+	analogWrite(RF, vel);
+	analogWrite(LF, faster);
+	analogWrite(RB, 0);
+	analogWrite(LB, 0);
+	delay(milliseconds);
+	analogWrite(RF, 0);
+	analogWrite(LF, 0);
+	analogWrite(RB, 0);
+	analogWrite(LB, 0);
+}
+
+unsigned long getFilteredDist(SharpIR long_sensor)
+{
+	int windowSize = 10;
+	// unsigned long distArr[windowSize];
+	unsigned long sum = 0;
+	for (int i = 0; i < windowSize; i++)
+	{
+		sum += long_sensor.getDistance();
+	}
+	return sum / windowSize;
+}
+void jukeTurn(int slowVel, int fastVel){
+	analogWrite(RB, slowVel);
+	analogWrite(LB, fastVel);
+	analogWrite(RF, 0);
+	analogWrite(LF, 0);
+
+}
---
+++ Arduino Test Code/simple_turn_and_push_bot/simple_turn_and_push_bot.ino
@@ -0,0 +1,279 @@
+
+#include <avr/io.h>
+#include "Arduino.h"
+// #include <SharpIR.h>
+#include "motor_functions.h"
+
+// define statements
+
+#define LONG_SENSOR_INPUT A7
+#define SHORT_SENSOR_INPUT 12
+#define START_SENSOR_INPUT A5
+#define LINE_SENSOR_FRONT_LEFT 7  // the digital input pin for line sensor 1
+#define LINE_SENSOR_FRONT_RIGHT 2 //'' for line sensor 2
+#define LINE_SENSOR_BACK_LEFT 4
+#define LINE_SENSOR_BACK_RIGHT 3
+// Global Variables
+unsigned int turnSpeed = 150;
+unsigned int fullSpeed = 255;
+unsigned int Max_Distance_longSensor = 80;
+unsigned long currentMillis = 0;
+unsigned long startMillis = 0;
+unsigned int distance = 0;
+bool startupSensorSeen = false;
+enum
+{
+    BEGINNING,
+    RUSH,
+    TURN_lEFT,
+    TURN_RIGHT,
+    JUKE,
+    REVERSE,
+    PUSH_STATE,
+    STOP,
+    TEST
+} state;
+// start off in the beginning, a very good place to start.
+
+int gabesSuperFunInt = 0;
+float gabesSuperFunFloat = 0.0;
+unsigned int pastValue = 0;
+unsigned int thisValue = 0;
+int diff = 0;
+
+// create new distance sensor object
+SharpIR long_sensor(SharpIR::GP2Y0A21YK0F, LONG_SENSOR_INPUT);
+
+void motor_functions_test()
+{
+    // test each one of the motor functions in turn
+    goForward(fullSpeed, 5000);
+    delay(1000);
+    turnLeft(90, turnSpeed);
+    delay(5000);
+    stopWheels();
+    delay(1000);
+    turnRight(90, turnSpeed);
+    delay(5000);
+    stopWheels();
+    delay(1000);
+    goBackward(fullSpeed, 5000);
+    delay(5000);
+    stopWheels();
+    delay(10000); // caution!!! final delay is long, but after ten seconds it will probably surprise you by running.
+}
+bool isFrontRightWhite()
+{
+    if (!digitalRead(LINE_SENSOR_FRONT_RIGHT))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool isFrontLeftWhite()
+{
+    if (!digitalRead(LINE_SENSOR_FRONT_LEFT))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool isBackLeftWhite()
+{
+    if (!digitalRead(LINE_SENSOR_BACK_LEFT))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool isBackRightWhite()
+{
+    if (!digitalRead(LINE_SENSOR_BACK_RIGHT))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool additionalDistanceFilter()
+{
+    thisValue = getFilteredDist(long_sensor);
+    if (thisValue < 50)
+    { // we have detected enemy robot!! Charge!!
+        delay(5);
+        diff = getFilteredDist(long_sensor) - thisValue;
+        if (abs(diff) <= 10)
+        {
+            return true;
+        }
+    }
+}
+
+void setup()
+{
+    state = BEGINNING;
+    Serial.begin(9600);
+    // Pins to configure.
+    // MOTOR PINS ALL ARE PWM PINS.
+    pinMode(RB, OUTPUT);
+    pinMode(RF, OUTPUT);
+    pinMode(LB, OUTPUT);
+    pinMode(LF, OUTPUT);
+    // SENSOR INPUT PINS
+    pinMode(SHORT_SENSOR_INPUT, INPUT);
+    pinMode(START_SENSOR_INPUT, INPUT);
+    pinMode(LINE_SENSOR_FRONT_LEFT, INPUT);
+    pinMode(LINE_SENSOR_FRONT_RIGHT, INPUT);
+    pinMode(LINE_SENSOR_BACK_LEFT, INPUT);
+    pinMode(LINE_SENSOR_BACK_RIGHT, INPUT);
+    pastValue = getFilteredDist(long_sensor);
+    stopWheels();
+    // lONG_SENSOR_INPUT is already configured by the object creation.
+    // This article explains how to remap the default pwm frequencies for arduino
+    // https://microcontrollerslab.com/arduino-pwm-tutorial-generate-fix-and-variable-frequency-signal/#:~:text=The%20analogWrite()%20function%20which,using%20the%20analogWrite()%20command.
+}
+
+void loop()
+{
+    // Begin the State Machine Layout
+    // distance = long_sensor.getDistance();
+    // Serial.println("Current State"state);
+
+    switch (state)
+    {
+    case BEGINNING:
+        Serial.println("begin");
+        if (!digitalRead(START_SENSOR_INPUT))
+        {
+            startupSensorSeen = true;
+            delay(5000); // we wait for 5 seconds
+        }
+        if (startupSensorSeen)
+        { // we've read the start sensor that wants us to drive!!
+            if ()
+            {
+                startMillis = millis();
+                goForward(255);
+                state = RUSH;
+            }
+            else
+            {
+                spinLeft(80);
+                state = TURN_lEFT;
+            }
+        }
+        break;
+    case RUSH:
+        Serial.println("RUSH");
+        currentMillis = millis();
+        if (getFilteredDist(long_sensor) > 50)
+        { // we have lost the enemy
+            spinLeft(80);
+            state = TURN_lEFT;
+        }
+        else if (getFilteredDist(long_sensor) < 10)
+        {
+            jukeTurn(25, 200);
+            startMillis = millis();
+            state = JUKE;
+        }
+
+        break;
+
+    case TURN_lEFT:
+        if (additionalDistanceFilter())
+        {
+            startMillis = millis();
+            goForward(255);
+            state = RUSH;
+        }
+
+        break;
+
+    case JUKE:
+        Serial.println("JUKE");
+        currentMillis = millis();
+        if (currentMillis - startMillis >= 50)
+        {
+            state = PUSH_STATE;
+            goBackward(255);
+        }
+        break;
+
+    case (REVERSE):
+        Serial.println("REVERSE");
+        currentMillis = millis();
+        if (currentMillis - startMillis >= 2000)
+        {
+            if (getFilteredDist(long_sensor) < 50)
+            { // we have detected enemy robot!! Charge!!
+                startMillis = millis();
+                goForward(155);
+                state = RUSH;
+                Serial.println("RUSH");
+            }
+            else
+            {
+                spinLeft();
+                state = TURN_lEFT;
+            }
+        }
+        break;
+
+    case PUSH_STATE:
+        // in this push state we need to check to see if we're heading off the edge.
+        if (isFrontLeftWhite() || isFrontRightWhite() || isBackLeftWhite() || isBackRightWhite())
+        {
+            goForward();
+            startMillis = millis();
+            state = RUSH;
+            delay(3000);
+            stopWheels();
+            state = BEGINNING;
+            break;
+        case STOP:
+            delay(5000);
+            state = BEGINNING;
+            break;
+        case TEST:
+            // goForward();
+            //  Serial.print("Front Left "); //THIS IS THE PROBLEMATIC ONE!!!
+            //  Serial.println(isFrontLeftWhite());
+            //  delay(100);
+            //  // Serial.print("Front Right ");
+            //  Serial.println(isFrontRightWhite());
+            //  delay(100);
+            //  // Serial.print("Back Left ");
+            //  Serial.println(isBackLeftWhite());
+            //  delay(100);
+            //  // Serial.print("Back Right ");
+            //  Serial.println(isBackRightWhite());
+
+            // if (isFrontRightWhite())
+            // {
+            //     escapeRightBack();
+            // }
+            // else if (isFrontLeftWhite())
+            // {
+            //     escapeLeftBack();
+            // }
+            // else
+            // {
+            //     goBackward();
+            // }
+            break;
+        default:
+            break;
+        }
+    }
